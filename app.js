6b23672a006ef52af10404ebef8f71ca68397456
Alex Cros <alexcroscuadros@gmail.com>
2018-07-03 22:12:00 +0200

    Router refactor. View engine to HTML. Error validation

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
+app.engine('html', require('ejs').__express);
+
+// Connect Mongoose
+//require('./lib/connectMongoose');
+
+app.locals.title = 'nodepop';
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,23 +22,54 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+/**
+ * API Routes
+ */
+
+//app.use('/apiv1/ads', require('./routes/apiv1/ads'));
+
+
+
+/**
+ * Application web routes
+ * 
+ */
+
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
+  // validation error
+  if (err.array) {
+    err.status = 422;
+    const errInfo = err.array({ onlyFirstError: true })[0];
+    err.message = `Not valid - ${errInfo.param} ${errInfo.msg}`;
+  }
+
+  // set error status
+  res.status(err.status || 500);
+
+  // JSON Respnse if is an API req
+  if (isAPI) {
+    res.json({ success: false, error: err.message});
+  }
+
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
   res.render('error');
 });
+
+function isAPI(req) {
+  return req.originalUrl.indexOf('/apiv') === 0;
+}
 
 module.exports = app;
