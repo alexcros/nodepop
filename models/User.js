5dd9d731d034c1bfa8d9eb63e889088e368488aa
Alex Cros <alexcroscuadros@gmail.com>
2018-07-16 18:32:32 +0200

    add createdRecord static method with validators and password hash

--- models/User.js
+++ models/User.js
@@ -11,6 +11,44 @@ const userSchema = mongoose.Schema({
     name: { type: String, index: true }
 });
 
+userSchema.statics.createRecord = function (addUser, callback) {
+    // validations
+    const valErrors = [];
+    //TODO: check name length
+    // if (!(v.isAlpha(addUser.name) && v.isLength(addUser.name, 2))) {
+    //     valErrors.push({ field: 'name', message: __('validation_invalid', { field: 'name' }) });
+    // }
+    if (!v.isEmail(addUser.email)) {
+        valErrors.push({ field: 'email', message: __('validation_invalid', { field: 'email' }) });
+    }
+
+    //TODO: check min chars ReferenceError: __ is not defined
+    //  if (!v.isLength(addUser.password, 6)) {
+    //      valErrors.push({ field: 'password', message: __('validation_invalid', { num: '6' }) });
+    //  }
+    if (valErrors.length > 0) {
+        return callback({ code: 422, errors: valErrors });
+    }
+
+    // verify duplicated email
+    User.findOne({ email: addUser.email }, function (err, user) {
+        if (err) {
+            return callback(err);
+        }
+
+        if (user) {
+            return callback({ code: 409, message: __('USER_EMAIL_DUPLICATED') });
+        } else {
+            let hashedPassword = crypto.createHash('sha256').update(addUser.password).digest('base64');
+            console.log('se ha hecho el hash', hashedPassword);
+            addUser.password = hashedPassword;
+
+            // create user
+            new User(addUser).save(callback);
+        }
+    });
+};
+
 // create model
 const User = mongoose.model('User', userSchema);
 
