1d8a0d7b0a58830418267518c7debc27d20584d0
Alex Cros <alexcroscuadros@gmail.com>
2018-07-16 18:05:42 +0200

    Add required libs and models

--- scripts/install_db.js
+++ scripts/install_db.js
@@ -1,9 +1,15 @@
 'use strict';
 
+const mongoose = require('mongoose');
+const readLine = require('readline');
+const async = require('async');
 
 // database connection
 const db = require('../lib/connectMongoose');
 
+// load defined models
+require('../models/User');
+require('../models/Ad');
 
 
     });
