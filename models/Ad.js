eccf820805e3334889522867c0f9e0fb742c85ed
Alex Cros <alexcroscuadros@gmail.com>
2018-07-16 18:21:04 +0200

    format

--- models/Ad.js
+++ models/Ad.js
@@ -20,32 +20,34 @@ const adSchema = mongoose.Schema({
 // filter list
 adSchema.statics.list = function (filter, skip, limit, fields, sort, tags) {
 
-    // create query with desired filters
-    const query = Ad.find(filter);
-    query.select(fields);
+  // create query with desired filters
+  const query = Ad.find(filter);
+  query.select(fields);
 
-    // add pagination
-    query.skip(skip);
-    query.limit(limit);
+  // add pagination
+  query.skip(skip);
+  query.limit(limit);
 
-    // db sort is executed before the pagination
-    query.sort(sort);
+  // db sort is executed before the pagination
+  query.sort(sort);
 
-    // execute query and return promise
-    return query.exec(tags);
+  // execute query and return promise
+  return query.exec(tags);
 }
 
 // filter by tag
 adSchema.statics.tagList = (tags) => {
-    const query = Ad.distinct('tags');
-    query.exec(tags);
+  const query = Ad.distinct('tags');
+  query.exec(tags);
 };
 
 /**
  * List of permitted tags
  */
 adSchema.statics.allowedTags = function () {
-    return ['work', 'lifestyle', 'motor', 'mobile'];
+  return ['work', 'lifestyle', 'motor', 'mobile'];
+};
+
 /**
  * load json ads
  */
